f7336d2afea731916f2478fe09d796419d3e7b4d
ant <anthonybelch05@gmail.com>
2025-11-06 00:42:03 -0800

    Initialize React application in index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './templates/FigmaDesign/App.jsx'; // make sure this path matches your actual file
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
